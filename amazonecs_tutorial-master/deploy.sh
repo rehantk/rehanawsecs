47a85ccf43f054f5ea69ba541a8b625f0f41cbd1
Rian <rian@ubuntu.ubuntu-domain>
2019-03-30 00:13:52 +0530

    my name updated in the task and service names in ecs

--- amazonecs_tutorial-master/deploy.sh
+++ amazonecs_tutorial-master/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-REGION=us-east-1
-SERVICE_NAME=tetra-app-service
-CLUSTER=tetranoodle-cluster
+REGION=ap-south-1b
+SERVICE_NAME=rehannoodle-app-service
+CLUSTER=rehannoodle-cluster
 IMAGE_VERSION="v_"${BUILD_NUMBER}
-TASK_FAMILY="tetra-app"
+TASK_FAMILY="rehannoodle-app"
 
 # Create a new task definition for this build
 
-sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ../tetra-app.json > tetra-app-v_${BUILD_NUMBER}.json
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ../rehannoodle-app.json >rehannoodle-app-v_${BUILD_NUMBER}.json
 
-aws ecs register-task-definition --family tetra-app --cli-input-json file://tetra-app-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family rehannoodle-app --cli-input-json file://rehannoodle-app-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
-REVISION=`aws ecs describe-task-definition --task-definition tetra-app | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+REVISION=`aws ecs describe-task-definition --task-definition rehannoodle-app | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 
 
